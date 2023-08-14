b8fcb61b33e95c65c0e293243806dd2385fe4178
Matthew Gracia <mlg8808@gmail.com>
2023-08-14 09:19:04 -0700

    Finished all models

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: Datatypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_names: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL(7, 4),
+      },
+    inStock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 33,
+      },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
